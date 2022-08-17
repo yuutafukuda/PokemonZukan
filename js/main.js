d2dc342ae0cd26940bf3ba60de5016130807af11
yuutafukuda <viyutafukuda@gmail.com>
2022-08-17 01:08:54 -0300

    Added script

--- js/main.js
+++ js/main.js
@@ -1,24 +1,63 @@
-// PokeAPIの使い方を「転生しないブログ」をみて覚えました
-// リンク https://tensei-shinai.com/2022/05/02/poke-api/
+// I've learned how to PokeAPI at「転生しないブログ」
+// https://tensei-shinai.com/2022/05/02/poke-api/
 
 const pokemonImg = document.querySelector('#pokemon__img');
 const pokemonNumber = document.querySelector('#pokemon__number');
 const pokemonName = document.querySelector('#pokemon__name');
 
+const form = document.querySelector('#form');
+const  input = document.querySelector('.input-search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`); 
-    const pokemonData = await APIResponse.json();
 
-    return pokemonData;
+    if (APIResponse.status === 200) { //verification of valid pokemon name
+        const pokemonData = await APIResponse.json();
+        return pokemonData;
+    }
 }
 
 const renderPokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading ...';
+    pokemonNumber.innerHTML = '';
+
     const pokemonData = await fetchPokemon(pokemon);
 
-    // pokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonData.id}.gif`
-    pokemonNumber.innerHTML = pokemonData.id ;
-    pokemonName.innerHTML = pokemonData.name;
-    pokemonImg.src = pokemonData['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if (pokemonData) {
+        pokemonNumber.innerHTML = pokemonData.id ;
+        pokemonName.innerHTML = pokemonData.name;
+        pokemonImg.src = pokemonData['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        pokemonImg.style.display = 'block';
+        searchPokemon = pokemonData.id;
+    } else {
+        pokemonName.innerHTML = 'Not found :(';
+        pokemonNumber.innerHTML = '';
+        pokemonImg.style.display = 'none';
+    }
+
+    input.value = '';
 }
 
-renderPokemon('pikachu');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1){
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    }
+});
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
